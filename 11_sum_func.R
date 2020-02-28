5c6e77224fbefa334c96f935a0664909d999a1b6
anneMehl <anne.mehlhoop@nina.no>
2020-02-28 15:56:53 +0100

    -

--- 11_sum_func.R
+++ 11_sum_func.R
@@ -1,23 +1,34 @@
+## load libraries ----
 library(raster)
 library(rgdal)
 
 
 ## load maps -----
-
-func_migr_b001_without_cross <- raster("maps/gardermoen_func_migratory_b001_without_cross.asc")
-func_migr_b001_with_cross <- raster("maps/gardermoen_func_migratory_b001_without_cross.asc")
-
-cellStats(func_migr_b001_without_cross, sum)
-cellStats(func_migr_b001_with_cross, sum)
-sum(values(p_stationary_without_cross_2702))
+func_migr_b001_without_cross_2802 <- raster("maps/func_migr_b001_without_cross_2802.asc")
+func_migr_b001_with_cross_2802 <- raster("maps/func_migr_b001_with_cross_2802.asc")
 
 
-plot(func_migr_b001_without_cross)
-str(func_migr_b001_without_cross)
-# ..@ ncols   : int 377
-# ..@ nrows   : int 425
+## plot maps ----
+plot(func_migr_b001_without_cross_2802) 
+plot(func_migr_b001_with_cross_2802)
 
 
+####### sum functuionality -------------------
+# stationary ----
+cellStats(func_stati_b001_without_cross_2802, sum) # [1] 42547849
+cellStats(func_stati_b001_with_cross_2802, sum) # [1] 43289952
+
+cellStats(func_stati_b001_with_cross_2802, sum) - cellStats(func_stati_b001_without_cross_2802, sum)
+# [1] 742103.1
+# > (742103.1/42547849)*100 ----> or do I have to use the larger number (func with cross)?
+# [1] 1.744161 %
 
 
+# migratory ----
+cellStats(func_migr_b001_without_cross_2802, sum) # [1] 153484358
+cellStats(func_migr_b001_with_cross_2802, sum) # [1] 153497768
 
+cellStats(func_migr_b001_with_cross_2802, sum) - cellStats(func_migr_b001_without_cross_2802, sum)
+# [1] 13409.49
+# > (13409.49/153484358)*100 ----> or do I have to use the larger number (func with cross)?
+# [1] 0.008736714 %
